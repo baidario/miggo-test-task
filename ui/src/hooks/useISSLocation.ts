88c91cf72bba181294026452dee07215c7b823b8
Roman Baida <baidario.ua@gmail.com>
2025-01-10 21:13:18 +0200

    Use express backend for API request

--- ui/src/hooks/useISSLocation.ts
+++ ui/src/hooks/useISSLocation.ts
@@ -5,14 +5,20 @@ export const useISSLocation = () => {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
 
   const loadCoordinates = useCallback(async () => {
-    const response = await fetch('http://api.open-notify.org/iss-now.json');
+    try {
+      const response = await fetch('http://localhost:3000/station');
 
-    if (response.ok) {
-      const json = await response.json() as {
-        iss_position: Coordinates;
-      };
+      if (response.ok) {
+        const json = await response.json() as {
+          iss_position: Coordinates;
+        };
 
-      setCoordinates(json.iss_position);
+        setCoordinates(json.iss_position);
+      }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      console.log('Error during coordinates loading');
     }
   }, []);
 
